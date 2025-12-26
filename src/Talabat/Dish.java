11ec6c66b7942022cbefcf823c6bfc3ec6b67c70
khaled <20241700285@cis.asu.edu.eg>
2025-12-27 01:51:21 +0200

    Dish First Commit

--- src/Talabat/Dish.java
+++ src/Talabat/Dish.java
@@ -1,4 +1,30 @@
 package Talabat;
 
 public class Dish {
+    private String name;
+    private String discription;
+    private String category;
+    private double price;
+    private int timesOrdered = 0 ;
+    private boolean visable = false ;
+
+    public Dish(){
+
+    }
+
+    public Dish(String name, String discription, String category, double price) {
+        this.name = name;
+        this.discription = discription;
+        this.category = category;
+        this.price = price;
+    }
+
+    public Dish(Dish dish){
+        this.name = dish.name;
+        this.discription = dish.discription;
+        this.category = dish.category;
+        this.price = dish.price;
+        this.timesOrdered = dish.timesOrdered;
+        this.visable = dish.visable;
+    }
 }
