4855c114debda06cade0c1a44322b1ca91124ceb
Youssef Ahmed Zaki <yousef.zaki384@gmail.com>
2025-12-27 21:13:58 +0200

    Rename OderItem to OrderItem and refactor code
    Refactor class name from 'OderItem' to 'OrderItem' and update constructors and methods accordingly.

--- src/Talabat/OderItem.java
+++ src/Talabat/OderItem.java
@@ -1,4 +1,51 @@
 package Talabat;
 
-public class OderItem {
+public class OrderItem {
+
+    private int quantity;
+    private double TotalPrice;
+
+    private Dish orderedDish;
+
+
+    public OrderItem(int quantity, Dish orderedDish) {
+        this.quantity = quantity;
+        this.orderedDish = orderedDish;
+    }
+    public OrderItem(){
+        this(0, null);
+    }
+
+    public OrderItem(OrderItem otherOrderItem) {
+//            this(otherOrderItem.quantity, new Dish(otherOrderItem.getOrderedDish())));
+
+    }
+
+    private void calculateTotalPrice() {
+//      this.TotalPrice = quantity * orderedDish.getPrice();
+    }
+
+    public void setOrderedDish(Dish orderedDish) {
+        if (orderedDish == null)
+            throw new IllegalArgumentException("Dish can't be null");
+
+        this.orderedDish = orderedDish;
+    }
+
+    public void setQuantity(int quantity) { this.quantity = quantity; }
+
+    public double getTotalPrice()  { return TotalPrice; }
+
+    public Dish getOrderedDish() { return orderedDish; }
+
+    public int getQuantity() { return quantity; }
+
+    @Override
+    public String toString() {
+        return //orderedDish.getName() +
+                "\t"  + getQuantity()
+                + "\t" + getTotalPrice();
+    }
 }
+
+
